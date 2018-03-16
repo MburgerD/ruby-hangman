846e9b46839cd2c99ff7cfbeb7938e9c3cd07af3
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-16 13:33:53 +1300

    update view tests

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,27 +3,36 @@ require_relative '../model.rb'
 
 
 RSpec.describe HangmanView do 
-  
-  describe "#print_word_progress" do
-    it "shows the letters which have been guessed correctly" do
-      hangman = HangmanModel.new(word='foo')
-      puts hangman.word
-      view = HangmanView.new(hangman)
-      expect{view.print_word_progress}.to output("***\n").to_stdout
-      hangman.correct_letters << "o"
-      expect{view.print_word_progress}.to output("*oo\n").to_stdout
+
+  describe "#print_status" do
+
+    context "with no letters guessed" do
+      it "reveals no letters in the word" do
+        hangman = HangmanModel.new(word="foo")
+        view = HangmanView.new(hangman)
+        expected_output = "\nhere's the word so far: "\
+                          "***\n"\
+                          "you have #{hangman.remaining_lives} lives remaining\n"
+        expect{view.print_status}.to output(expected_output).to_stdout
+      end
+    end    
+
+    context "with correct letters guessed" do
+      it "reveals correct letters in the word and used letters" do
+        hangman = HangmanModel.new(word="foo")
+        view = HangmanView.new(hangman)
+
+        hangman.correct_letters.push("o")
+        hangman.used_letters.push("a", "b")
+
+        expected_output = "\nhere's the word so far: "\
+                          "*oo\n"\
+                          "you have #{hangman.remaining_lives} lives remaining\n"\
+                          "Letters you've guessed: a b\n"
+        expect{view.print_status}.to output(expected_output).to_stdout
+      end
     end
+    
   end
 
-  describe "#print_used_letters" do
-    it "shows the letters which have been used" do
-      hangman = HangmanModel.new
-      puts hangman.word
-      view = HangmanView.new(hangman)
-      expect{view.print_used_letters}.to output("").to_stdout
-      hangman.used_letters.push("a", "b")
-      expect{view.print_used_letters}.to output("Letters you've guessed: a b\n").to_stdout
-    end
-  end
-
-end
+end 
